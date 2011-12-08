e8afd21078f4a5c17a98319f6b7c8281504ba942
Ratnam Raju <ratnamraju.v@nyros.in>
2011-12-08 17:46:58 +0530

    initial comit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+	
+	def index
+		 render :layour=> false
+	end
+	def new
+	end
+	
+	def create
+	end
+	
+	def show
+	end
+	
+	def edit
+	end
+	
+	def update
+	end
+	
+	def destory
+	end
+	
+ 
+		
+	
+	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aaron::Application.config.secret_token = '4a6dda113ba74a7c3764e30caa1026b2380d7fe98341e6704f5f90ccd6e8d41db79d88d1978e3de8c2e0b13c4c5d227d3b895809e41ac37faff6aa8b37a5a9f9'
